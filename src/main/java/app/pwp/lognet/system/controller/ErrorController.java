db2fbeecf36460cced4e5932c6ab7ded0abb5e72
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-24 18:33:25 +0800

    fix: minor changes

--- src/main/java/app/pwp/lognet/system/controller/ErrorController.java
+++ src/main/java/app/pwp/lognet/system/controller/ErrorController.java
@@ -13,25 +13,25 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
     public R handleError(HttpServletRequest request) {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         if (statusCode == 500) {
-            return R.error("Unknown internal error occurred.");
+            return R.error("发生了一个未知的服务器错误");
         } else if (statusCode == 401) {
-            return R.unauth("Login first.");
+            return R.unauth("请先登录");
         } else if (statusCode == 403) {
-            return R.forbidden("You're not allowed to access here.");
+            return R.forbidden("您没有权限访问");
         } else if (statusCode == 404) {
-            return R.noufound();
+            return R.notfound();
         } else {
-            return R.error("Unknown internal error occurred.");
+            return R.error("发生了一个未知的服务器错误");
         }
     }
 
     @RequestMapping("/auth")
     public R handleAuthError() {
-        return R.unauth("Login first.");
+        return R.unauth("请先登录");
     }
 
     @Override
     public String getErrorPath() {
-        return "/error";
+        return "/error/";
     }
 }
--- src/main/java/app/pwp/lognet/system/controller/PingController.java
+++ src/main/java/app/pwp/lognet/system/controller/PingController.java
@@ -1,13 +1,15 @@
 package app.pwp.lognet.system.controller;
 
 import app.pwp.lognet.utils.common.R;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/ping")
 public class PingController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public R ping() {
-        return R.ok();
+        return R.success();
     }
 }
