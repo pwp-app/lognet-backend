5abd1806a73fe37a6d02563f0caddb346eae5e55
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-24 18:36:34 +0800

    refactor: recaptcha is component

--- src/main/java/app/pwp/lognet/utils/auth/ReCaptcha.java
+++ src/main/java/app/pwp/lognet/utils/auth/ReCaptcha.java
@@ -1,4 +1,4 @@
-package app.pwp.lognet.system.service;
+package app.pwp.lognet.utils.auth;
 
 import app.pwp.lognet.config.common.RecaptchaConfig;
 import app.pwp.lognet.utils.network.Http;
@@ -7,14 +7,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
 
-@Service
-public class RecaptchaService {
+@Component
+public class ReCaptcha {
     @Resource
     private RecaptchaConfig recaptchaConfig;
 
