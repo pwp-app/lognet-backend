ca23e429fab7e038075a7d5e216806a7dd8c533b
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-27 22:04:18 +0800

    fix: jpa annotation

--- src/main/java/app/pwp/lognet/base/model/BaseEntity.java
+++ src/main/java/app/pwp/lognet/base/model/BaseEntity.java
@@ -2,11 +2,13 @@ package app.pwp.lognet.base.model;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity extends Base {
     @CreatedDate
     private Date createTime;
