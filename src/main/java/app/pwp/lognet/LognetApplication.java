e2c91fb199dd090e14e9449dbe7c11ab45128e04
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-27 22:04:33 +0800

    fix: jpa auditing

--- src/main/java/app/pwp/lognet/LognetApplication.java
+++ src/main/java/app/pwp/lognet/LognetApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
+@EnableJpaAuditing
 public class LognetApplication {
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
