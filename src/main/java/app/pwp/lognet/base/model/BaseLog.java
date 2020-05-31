6be800dfdb7b13c052094577a302ac9b3be47ae0
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-01 02:44:46 +0800

    fix: log date

--- src/main/java/app/pwp/lognet/base/model/BaseLog.java
+++ src/main/java/app/pwp/lognet/base/model/BaseLog.java
@@ -1,12 +1,23 @@
 package app.pwp.lognet.base.model;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseLog extends Base{
     @CreatedDate
     private Date createTime;
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 }
